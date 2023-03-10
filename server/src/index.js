ad9619e302fceec34f59a2636a6b3e8802fb50e0
shivang <shivangnov@gmail.com>
2023-03-10 19:02:48 +0530

    added announcement api

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 const dotenv= require('dotenv');
-const userRoutes = require('./routes/user_routes')
+const userRoutes = require('./routes/user_routes');
+const announcementRoutes = require('./routes/announcement_routes');
 dotenv.config();
 const {connectDB} = require('./database/database');
 app.listen(PORT, () => {
@@ -11,3 +12,4 @@ app.listen(PORT, () => {
 app.use(express.json());
 connectDB();
 app.use('/api/users',userRoutes);
+app.use('/api/announcements',announcementRoutes);
