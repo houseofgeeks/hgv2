c097141ef29d0f57027f5a5630fc0bd485014ad1
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-03-10 22:18:52 +0530

    adding footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import {Route,Routes} from 'react-router-dom'
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />}  />
         </Routes>
+      <Footer/>
     </div>
   );
 }
