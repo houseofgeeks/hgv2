5f42b92a4824774b0aaa9fedcef330ee736812f0
anubhav <anubhavshivahre@gmail.com>
2023-03-11 18:24:58 +0530

    added wing routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routes/user_routes");
 const announcementRoutes = require("./routes/announcement_routes");
 const feedRoutes = require("./routes/feed_routes");
+const wingRoutes = require("./routes/wing_routes");
 const { connectDB } = require("./database/database");
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const options = {
   origin: ["http://localhost:3000", "https://hgv2.vercel.app/"],
@@ -16,12 +18,14 @@ app.use(cors(options));
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 connectDB();
 
 app.use("/api/users", userRoutes);
 app.use("/api/feeds", feedRoutes);
+app.use("/api/wings", wingRoutes);
 
 app.use("/api/announcements", announcementRoutes);
--- server/models/wing_model.js
+++ server/models/wing_model.js
@@ -11,7 +11,7 @@ const wingSchema = new mongoose.Schema(
     },
     image: {
       type: String,
-      required: true,
+      //required: true,
     },
     levels: [
       {
