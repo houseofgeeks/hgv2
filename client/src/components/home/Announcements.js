6827787f8e543567d93ab02f69effb02ab29038b
ritik jha <coolritikjha46@gmail.com>
2023-05-13 12:40:29 +0530

    changes

--- client/src/components/home/Announcements.js
+++ client/src/components/home/Announcements.js
@@ -5,17 +5,17 @@ const Announcements = ({ feed, details }) => {
   return (
     <div
       className={`xs:min-h-[400px] sm:ml-10 rounded-lg bg-[#0F1010] p-5 ${
-        feeds && "mt-10"
+        feed && "mt-10"
       }`}
     >
-      {feeds ? (
+      {feed ? (
         <h1 className="text-slate-300 text-xl">HG Feed</h1>
       ) : (
         <h1 className="text-slate-300 text-xl">HG Announcements</h1>
       )}
-      {feeds ? (
+      {feed ? (
         <div className="mt-4">
-          {feeds?.map((items, i) => {
+          {feed?.map((items, i) => {
             if (i < 5) {
               return <AnnounceSection key={i} feed items={items} />;
             }
