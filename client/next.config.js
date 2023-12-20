f99b5c4595a7d9bebf152e08c41905025904fde1
Sachin <kumaradity7125@gmail.com>
2023-12-20 18:37:19 +0530

    home ui fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
