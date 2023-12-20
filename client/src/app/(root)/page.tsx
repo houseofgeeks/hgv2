e664fe4f627166694240acbfdc446fc2326fd22d
Sachin <kumaradity7125@gmail.com>
2023-12-20 18:35:50 +0530

    home ui fix

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -3,8 +3,8 @@ import Rightbar from "../../components/home/Rightbar";
 
 const Home = () => {
   return (
-    <div className="px-12 ss:px-24 min-h-[100vh] py-5 bg-[#010203]">
-      <div className="flex sm:flex-row flex-col">
+    <div className="px-8 sm:px-10 md:px-24 min-h-[100vh] py-5 bg-[#010203]">
+      <div className="flex sm:flex-row flex-col-reverse">
         <YourActivity />
         <Rightbar />
       </div>
--- client/src/components/home/Rightbar.tsx
+++ client/src/components/home/Rightbar.tsx
@@ -1,8 +1,4 @@
 import Announcements from "./Announcements";
-// import { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { fetchAnnouncements, fetchFeeds } from "../../redux/features/homeSlice";
-import Loader from "../loader/Loader";
 
 interface RightbarProps {
   announcements?: string;  // it will be an array added string for now
@@ -11,41 +7,63 @@ interface RightbarProps {
   error: any;
 }
 
+const feeds=[
+  {
+    id:1,
+    feedDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem. ",
+    upVotes:[1,2,3,4,5]
+  },
+  {
+    id:2,
+    feedDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.",
+    upVotes:[1,2,3,4,5]
+  },
+  {
+    id:3,
+    feedDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.",
+    upVotes:[1,2,3,4,5]
+  },
+  {
+    id:4,
+    feedDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.",
+    upVotes:[1,2,3,4,5]
+  }
+]
+
+const details=[
+  {
+    id:1,
+    announcementDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem",
+  },
+  {
+    id:2,
+    announcementDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.",
+  },
+  {
+    id:3,
+    announcementDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem",
+  },
+  {
+    id:4,
+    announcementDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.",
+  },
+  {
+    id:5,
+    announcementDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.",
+  },
+  {
+    id:6,
+    announcementDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.",
+  }
+]
+
+
 const Rightbar = () => {
-  //   const {
-  //     announcements,
-  //     feeds,
-  //     loading,
-  //     // error
-  //   } = useSelector((state) => state.home);
-  //   const dispatch = useDispatch();
-
-  //   useEffect(() => {
-  //     dispatch(fetchAnnouncements());
-  //     dispatch(fetchFeeds());
-  //   }, [dispatch]);
-
-  //   if (loading) {
-  //     return (
-  //       // <div class="w-60 h-24 border-2 rounded-md mx-auto mt-20">
-  //       //   <div class="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
-  //       //     <div class="w-12 bg-gray-300 h-12 rounded-full "></div>
-  //       //     <div class="flex flex-col space-y-3">
-  //       //       <div class="w-36 bg-gray-300 h-6 rounded-md "></div>
-  //       //       <div class="w-24 bg-gray-300 h-6 rounded-md "></div>
-  //       //     </div>
-  //       //   </div>
-  //       // </div>
-  //       // <div>Loading...</div>
-  //       <Loader />
-  //     );
-  //   }
-
   return (
     <div className="flex-[0.75] mt-8 sm:mt-0">
       <div>
-        <Announcements details={["announcement1", "announcement2"]} />
-        <Announcements feeds={["feed1", "feed2"]} />
+        <Announcements details={details} />
+        <Announcements feeds={feeds} />
       </div>
     </div>
   );
