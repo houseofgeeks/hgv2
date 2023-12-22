47429039c1bb6a736692a5b1d2285f3d47c8e492
aialok <alokgupta1560@gmail.com>
2023-12-22 23:13:15 +0530

    Initial Setup Done

--- server/routes/subtopic_routes.js
+++
@@ -1,7 +0,0 @@
-const router=require("express").Router();
-const {checkAdmin}=require("../middlewares/authMiddleware");
-const {createSubtopic,addResource}=require("../controllers/subtopic_controller");
-
-router.route("/").post(checkAdmin,createSubtopic);
-router.route("/addResource").patch(checkAdmin,addResource);
-module.exports=router;
